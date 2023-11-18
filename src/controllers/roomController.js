e9539d3d24d9f6cdfdad8bb4996d9722569bcf56
raphagoo <raphael.agostini@ynov.com>
2023-11-18 16:59:04 +0100

    suite refacto mongoose

--- src/controllers/roomController.js
+++ src/controllers/roomController.js
@@ -6,12 +6,11 @@ const Room = mongoose.model('Room', RoomSchema);
 export const createRoom = (req, res) => {
     //if(verifyJwt(req) === true){
         let newRoom = new Room(req.body);
-        newRoom.save((err, room) => {
-            if(err) {
-                res.status(400).send(err);
-            } else {
-                res.status(201).json(room);
-            }
+        newRoom.save()
+        .then((room) => {
+            res.status(201).json(room);
+        }).catch((err) => {
+            res.status(400).send(err);
         })
     //}
     //else{
@@ -21,55 +20,48 @@ export const createRoom = (req, res) => {
 
 export const listRooms = (req, res) => {
     Room.find({})
-    .then((rooms, err) => {
-        if(rooms) {
-            res.status(200).json(rooms)
-        } else {
-            res.status(400).send(err);
-        }
+    .then((rooms) => {
+        res.status(200).json(rooms)
+    }).catch((err) => {
+        res.status(400).send(err);
     });
 };
 
 export const getRoom = (req, res) => {
     Room.findById(req.params.id)
-    .then((room, err) => {
+    .then((room) => {
         if(room) {
             res.status(200).json(room)
         } else if(room == null) {
             res.sendStatus(404)
-        } else {
-            res.status(400).send(err);
         }
+    }).catch((err) => {
+        res.status(400).send(err);
     });
 };
 
 export const updateRoom = (req, res) => {
     Room.findOneAndUpdate({"_id": req.params.id}, req.body, {new: true, useFindAndModify: false})
-    .then((room, err) => {
-        if(err) {
+    .then((room) => {
+        if(room) {
             res.status(200).json(room);
-        } else {
-            if(room == null) {
-                res.sendStatus(404);
-            }
-            else {
-                res.status(400).send(err);
-            }
+        } else if(room == null) {
+            res.sendStatus(404);
         }
+    }).catch((err) => {
+        res.status(400).send(err);
     });
 };
 
 export const deleteRoom = (req, res) => {
-    Room.findOneAndDelete({"_id": req.params.id}, (err, room) => {
-        if(err) {
-            res.status(400).send(err);
-        } else {
-            if(room == null) {
-                res.sendStatus(404);
-            }
-            else {
-                res.sendStatus(204);
-            }
+    Room.findOneAndDelete({"_id": req.params.id})
+    .then((room) => {
+        if(room) {
+            res.sendStatus(204);
+        } else if (room == null) {
+            res.sendStatus(404);
         }
+    }).catch((err) => {
+        res.status(400).send(err);
     });
 };
