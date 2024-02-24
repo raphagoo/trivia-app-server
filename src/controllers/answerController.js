cbcbd0cfe3aa3092a5fc9438a209f5b530fe2187
Raphaël Agostini <33906573+raphagoo@users.noreply.github.com>
2024-02-24 17:22:51 +0100

    script missing answers (#21)

--- src/controllers/answerController.js
+++ src/controllers/answerController.js
@@ -6,15 +6,41 @@ const Answer = mongoose.model('Answer', AnswerSchema);
 export const checkAnswer = (payload) => {
     return new Promise((resolve, reject) => {
         Answer.findById(payload.answer._id)
-            .then((answer) => {
-                if (answer) {
-                    resolve(answer.correct);
-                } else {
-                    resolve(false);
+        .then((answer) => {
+            Answer.find({question: answer.question._id})
+            .then((answers) => {
+                let enteredAnswer = answers.find((answer) => {
+                    return answer._id == payload.answer._id;
+                })
+                if(enteredAnswer.correct) {
+                    resolve({answerCorrectId: enteredAnswer._id, correct: enteredAnswer.correct});
+                }
+                else {
+                    let correctAnswer = answers.find((answer) => {
+                        return answer.correct;
+                    })
+                    resolve({answerCorrectId: correctAnswer._id, correct: enteredAnswer.correct});
                 }
             })
-            .catch((err) => {
-                reject(err);
-            });
+        })
+        // Answer.find({question: payload.question._id})
+        //     .then((answers) => {
+        //         let enteredAnswer = answers.find((answer) => {
+        //             return answer.answer === payload.answer;
+        //         })
+        //         console.log(enteredAnswer)
+        //         if(enteredAnswer.correct) {
+        //             resolve({answerCorrectId: enteredAnswer._id, correct: enteredAnswer.correct});
+        //         }
+        //         else {
+        //             let correctAnswer = answers.find((answer) => {
+        //                 return answer.correct;
+        //             })
+        //             resolve({answerCorrectId: correctAnswer._id, correct: enteredAnswer.correct});
+        //         }
+        //     })
+        //     .catch((err) => {
+        //         reject(err);
+        //     });
     });
 };
