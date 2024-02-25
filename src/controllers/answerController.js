0da9e540a5932984604d6b6c28b9a424abb5efc7
raphagoo <raphael.agostini@ynov.com>
2024-02-25 19:25:15 +0100

    clean checkAnswer

--- src/controllers/answerController.js
+++ src/controllers/answerController.js
@@ -20,24 +20,8 @@ export const checkAnswer = (payload) => {
                 resolve({answerCorrectId: correctAnswer._id, correct: false});
             }
         })
-        // Answer.find({question: payload.question._id})
-        //     .then((answers) => {
-        //         let enteredAnswer = answers.find((answer) => {
-        //             return answer.answer === payload.answer;
-        //         })
-        //         console.log(enteredAnswer)
-        //         if(enteredAnswer.correct) {
-        //             resolve({answerCorrectId: enteredAnswer._id, correct: enteredAnswer.correct});
-        //         }
-        //         else {
-        //             let correctAnswer = answers.find((answer) => {
-        //                 return answer.correct;
-        //             })
-        //             resolve({answerCorrectId: correctAnswer._id, correct: enteredAnswer.correct});
-        //         }
-        //     })
-        //     .catch((err) => {
-        //         reject(err);
-        //     });
+        .catch((err) => {
+            reject(err);
+        });
     });
 };
