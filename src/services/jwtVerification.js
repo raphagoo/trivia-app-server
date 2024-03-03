4c75923a607b46ccb301548c09cfa09c76a068be
raphagoo <raphael.agostini@ynov.com>
2024-03-03 19:05:41 +0100

    login & register normal user + refresh

--- src/services/jwtVerification.js
+++ src/services/jwtVerification.js
@@ -1,13 +1,14 @@
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 
 export function verifyJwt(req){
     let tokenVerified = false
     let token = req.headers['x-access-token'] || req.headers['authorization']; // Express headers are auto converted to lowercase
     if (token.startsWith('Bearer ')) {
       // Remove Bearer from string
-      token = token.slice(7, token.length);
+        token = token.slice(7, token.length);
     }
-    jwt.verify(token, 'mySuperSecrett', function(err, decoded){
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
         if(err){
             tokenVerified = false
         }
