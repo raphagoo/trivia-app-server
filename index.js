98022c37b91377069efa23f1791f7cd302899389
Raphaël Agostini <33906573+raphagoo@users.noreply.github.com>
2024-04-27 20:52:22 +0200

    dynamic cors origin socket (#38)

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:8080",
+        origin: process.env.FRONTEND_URL,
         methods: ["GET", "POST"]
     }
 });
