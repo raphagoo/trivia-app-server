2b8de8180911b985c50ce18f83f9692a7923aa9f
raphagoo <raphael.agostini@ynov.com>
2024-05-19 17:46:41 +0200

    CI, test db with fixtures

---
+++ fixtures/fixtures.js
@@ -0,0 +1,12 @@
+export default {
+    users: [
+        {
+            username: 'user 1',
+            password: 'password'
+        },
+        {
+            username: 'user 2',
+            password: 'password'
+        }
+    ]
+};
