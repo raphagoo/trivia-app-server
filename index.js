580778b485fba3a51bd7223959bd0324e475c095
raphagoo <raphael.agostini@ynov.com>
2024-06-08 16:12:42 +0200

    specify port

--- index.js
+++ index.js
@@ -131,6 +131,6 @@ io.on('connection', (socket) => {
 
 
 server.listen(process.env.PORT || 3000,
-    console.log(`listening`)
+    console.log('Server running on port' + process.env.PORT || 3000),
 );
 
